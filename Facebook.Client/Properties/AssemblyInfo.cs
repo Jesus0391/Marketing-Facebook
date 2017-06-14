c5be3d38926d3ba9efad9b8440bc719f401f63f6
Jesus Mendez <Jesus.r.mendezp@gmail.com>
2017-06-14 15:00:48 -0400

    +Split in Folder Solutions for Frameworks and Core Project and remove projects.

--- Facebook.Client/Properties/AssemblyInfo.cs
+++ Facebook.Client/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Facebook.Models")]
-[assembly: AssemblyDescription("Models for Facebook API")]
+[assembly: AssemblyTitle("Facebook.Client")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Facebook.Models")]
+[assembly: AssemblyProduct("Facebook.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64516afb-1086-4f3e-b60c-4d1ffb64bbe0")]
 
 // Version information for an assembly consists of the following four values:
 //
